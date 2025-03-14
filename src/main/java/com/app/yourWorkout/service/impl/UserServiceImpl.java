569c1febea23e723ac02ef0eea4bb02bcfef6d54
Lucas Terra <lucasterracontato@gmail.com>
2025-03-14 14:09:17 -0300

    fix implementation on user service

--- src/main/java/com/app/yourWorkout/service/impl/UserServiceImpl.java
+++ src/main/java/com/app/yourWorkout/service/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.app.yourWorkout.service.impl;
 
+import com.app.yourWorkout.DTO.request.user.UserCreateRequest;
 import com.app.yourWorkout.DTO.request.user.UserUpdateRequest;
 import com.app.yourWorkout.DTO.response.UserReadResponse;
 import com.app.yourWorkout.entities.User;
@@ -74,14 +75,18 @@ class UserServiceImpl implements UserService {
 
     @Override
     @Transactional
-    public UserReadResponse saveUser(String username, String email, String password) {
-        if (userRepository.existsByName(username)) {
-            throw new DuplicateDataException("The user with username: " + username + " already exists in the database");
+    public UserReadResponse saveUser(UserCreateRequest userCreateRequest) {
+        if (userRepository.existsByName(userCreateRequest.name())) {
+            throw new DuplicateDataException("The user with username: " + userCreateRequest.name() + " already exists in the database");
         }
 
         return UserReadResponse.from(
                 userRepository.save(
-                        new User(username,email,password)
+                        new User(
+                                userCreateRequest.name(),
+                                userCreateRequest.email(),
+                                userCreateRequest.password()
+                        )
                 )
         );
     }
